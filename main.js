bd2a56d03bf35f65c764f7573171e5d1e16e1778
rgianan@ched.gov.ph <rgianan@ched.gov.ph>
2026-01-07 22:26:57 +0800

    version 2

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ let tie = 0;
 let cpuPick = '';
 let result = '';
 
-function rock () {
+function roll () {
 const numberRandom = Math.random();
  if (numberRandom >= 0 && numberRandom <= 1/3)
       {
@@ -20,83 +20,49 @@ const numberRandom = Math.random();
       cpuPick = 'Scissors' ; 
       }
     console.log(numberRandom)
+}
 
-  if (cpuPick === 'Rock') {
-    result = 'Tie.';
-    tie++;
-  } else if (cpuPick === 'Paper') {
-    result = 'You lose.';
-    cpuScore++;
-  } else if (cpuPick === 'Scissors') {
-    result = 'You win.';
-    playerScore++;
+function pick (playerPick) {
+  roll();
+  if (playerPick === 'rock')
+    if (cpuPick === 'Rock') {
+      result = 'Tie.';
+      tie++;
+    } else if (cpuPick === 'Paper') {
+      result = 'You lose.';
+      cpuScore++;
+    } else if (cpuPick === 'Scissors') {
+      result = 'You win.';
+      playerScore++;
+  } else if  (playerPick === 'paper') {
+    if (cpuPick === 'Rock') {
+      result = 'You win.';
+      playerScore++;
+    } else if (cpuPick === 'Paper') {
+      result = 'Tie.';
+      tie++;
+    } else if (cpuPick === 'Scissors') {
+      result = 'You lose.';
+      cpuScore++;
+    }
+  } else if (playerPick === 'scissors') {
+      if (cpuPick === 'Rock') {
+      result = 'You lose.';
+      cpuScore++;
+    } else if (cpuPick === 'Paper') {
+      result = 'You win.';
+      playerScore++;
+    } else if (cpuPick === 'Scissors') {
+      result = 'Tie.';
+      tie++;
+    }
   }
 
-  alert(`You picked Rock. Computer picked ${cpuPick}. ${result}`);
+  alert(`You picked ${playerPick}. Computer picked ${cpuPick}. ${result}`);
+  dispResult();
+}
+function dispResult () {
   document.getElementById('ps').textContent = playerScore;
   document.getElementById('cs').textContent = cpuScore;
   document.getElementById('t').textContent = tie;
 }
-function paper() {
-const numberRandom = Math.random();
- if (numberRandom >= 0 && numberRandom <= 1/3)
-      {
-      cpuPick = 'Rock';
-      } 
-      else if (numberRandom > 1/3 && numberRandom < 2/3)
-      {
-      cpuPick = 'Paper';
-      }
-      else if (numberRandom < 1 && numberRandom >= 2/3)
-      {
-      cpuPick = 'Scissors';
-      }
-    console.log(numberRandom)
-
-  if (cpuPick === 'Rock') {
-    result = 'You win.';
-    playerScore++;
-  } else if (cpuPick === 'Paper') {
-    result = 'Tie.';
-    tie++;
-  } else if (cpuPick === 'Scissors') {
-    result = 'You lose.';
-    cpuScore++;
-  }
-  alert(`You picked Paper. Computer picked ${cpuPick}. ${result}`);
-  document.getElementById('ps').textContent = playerScore;
-  document.getElementById('cs').textContent = cpuScore;
-  document.getElementById('t').textContent = tie;
-}
-function scissors () {
-  const numberRandom = Math.random();
- if (numberRandom >= 0 && numberRandom <= 1/3)
-      {
-      cpuPick = 'Rock';
-      } 
-      else if (numberRandom > 1/3 && numberRandom < 2/3)
-      {
-      cpuPick = 'Paper';
-      }
-      else if (numberRandom < 1 && numberRandom >= 2/3)
-      {
-      cpuPick = 'Scissors';
-      }
-    console.log(numberRandom)
-
-  if (cpuPick === 'Rock') {
-    result = 'You lose.';
-    cpuScore++;
-  } else if (cpuPick === 'Paper') {
-    result = 'You win.';
-    playerScore++;
-  } else if (cpuPick === 'Scissors') {
-    result = 'Tie.';
-    tie++;
-  }
-  alert(`You picked Scissors. Computer picked ${cpuPick}. ${result}`);
-  document.getElementById('ps').textContent = playerScore;
-  document.getElementById('cs').textContent = cpuScore;
-  document.getElementById('t').textContent = tie;
-}
-
